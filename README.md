c68b0f97806f26a72e7ef5bb8280d8fb18a3b779
lkam <luckm7777@gmail.com>
2021-01-16 00:00:33 +0100

    Updates

--- README.md
+++ README.md
@@ -3,5 +3,14 @@
 - kubectl
 - Helm
 
-# Install
+# Steps
+1. Run vagrant
+vagrant up
+
+2. Install local helm chart
 helm install myrevproxy myrevproxy/ --values myrevproxy/values.yaml --kubeconfig shared/kubeconfig
+
+3. curl local machine IP with its NodePort
+curl 192.168.65.100:32222
+curl 192.168.65.101:32222
+curl 192.168.65.102:32222
--- scripts/install-all.sh
+++ scripts/install-all.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
-# Generic Ubuntu update and installs
-sudo apt-get update
-sudo apt-get install -y apt-transport-https curl
 # Install Docker using convenience scripts 
 # https://docs.docker.com/engine/install/ubuntu/#install-using-the-convenience-script
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sudo sh get-docker.sh
+# Install Docker from repository
+sudo apt-get update
+sudo apt-get install -y\
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 # Install Kubeadm 
 # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
